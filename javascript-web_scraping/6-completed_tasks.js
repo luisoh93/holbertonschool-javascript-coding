764ec282531d9d5b7b83682a7de1568840df5142
luisoh93 <luis_oh@hotmail.com>
2023-09-19 16:24:52 -0700

    Semistandard validation

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-	    const todos = JSON.parse(body);
+  const todos = JSON.parse(body);
 	    let completed = {};
 	    todos.forEach((todo) => {
 		      if (todo.completed && completed[todo.userId] === undefined) {
